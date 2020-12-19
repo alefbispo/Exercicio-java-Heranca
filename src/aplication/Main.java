25fddedefee1ee322d111f1fde7ac23c819f2523
Alef Bispo <alef.b.dos.santos@hotmail.com>
2020-12-19 17:34:44 -0300

    criando projeto

---
+++ src/aplication/Main.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("ola mundo");
+
+        scan.close();
+
+    }
+}
